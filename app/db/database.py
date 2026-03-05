3e6ef2eaaf46e106e62217a61ff08d74491d2e39
Krish <krishc4005@gmail.com>
2026-03-05 15:08:43 +0530

    Added standard database setup with SQLAlchemy and async support.

---
+++ app/db/database.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+DATABASE_URL =  os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set.")
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo = True
+)
+
+async_session_factory = async_sessionmaker(
+    engine, 
+    expire_on_commit = False
+)
+
+async def create_db_and_tables():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_factory() as session:
+        yield session
+# is this fine though?
