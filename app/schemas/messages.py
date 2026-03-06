67660011631b633b42476d5757f5cb9f01636851
Krish <krishc4005@gmail.com>
2026-03-06 20:34:56 +0530

    Corrected some issues in the comments and added a default value for the username field in UserUpdate schema.

--- app/schemas/messages.py
+++ app/schemas/messages.py
@@ -12,4 +12,4 @@ class MessageRead(BaseModel):
     content : str
     created_at : datetime
     
-    model_config = {"from_attributes": True} # yet to know what this does
+    model_config = {"from_attributes": True}
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -16,11 +16,24 @@ class UserRead(BaseModel):
     username : str
     email : EmailStr
 
-    model_config = {"from_attributes": True} # yet to know what this does
+    model_config = {"from_attributes": True} 
+    # What this line does:
+    # When we return a SQLAlchemy object from a FastAPI route, FastAPI uses the
+    # response_model (a Pydantic model) to validate and serialize the response.
+
+    # However, Pydantic normally expects dictionary-like data for validation.
+    # A SQLAlchemy model is an object with attributes (user.id, user.username).
+
+    # By enabling "from_attributes = True", we tell Pydantic that it should read
+    # values from object attributes instead of dictionary keys.
+
+    # This allows Pydantic to convert the SQLAlchemy object into a Pydantic model,
+    # and FastAPI then serializes that Pydantic model into JSON for the client.           
 
 class UserUpdate(BaseModel):
     username : str | None = Field(
         min_length = 3,
         max_length = 20,
-        pattern = "^[a-zA-Z0-9_]+$"
+        pattern = "^[a-zA-Z0-9_]+$",
+        default = None
     )
