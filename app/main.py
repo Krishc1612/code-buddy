a6881b739a76e377e575cb9d7da9c6dbcc2a7fe6
Krish <krishc4005@gmail.com>
2026-03-11 12:11:11 +0530

    Made the basic main file for the application for testing.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.api.routes import auth
+from app.db.database import create_db_and_tables
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(
+    title = "Code-buddy API",
+    version = "1.0.0",
+    lifespan = lifespan
+)
+
+app.include_router(auth.router, prefix = "/api", tags = ["auth"])
 
 @app.get('/')
-def root():
-    return {"message" : "Hello!"}
+async def root():
+    return {
+        "status" : "Code-budy API running"
+    }
