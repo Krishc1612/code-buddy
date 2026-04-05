7583daf179e89d843f9f63bf4364baa9799d32af
Krish <krishc4005@gmail.com>
2026-04-05 21:49:59 +0530

    Removed reload option from uvicorn.run in main.py for production deployment.

--- backend/main.py
+++ backend/main.py
@@ -45,4 +45,4 @@ app.include_router(chats.router, prefix="/api", tags=['chats'])
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT)
